a923f63fc9b2d3918cdcee35bc4e69578ca29891
gaarangoa <gaarangoa@gmail.com>
2017-11-03 17:00:12 -0400

    fix

---
+++ build/lib/deepGene/train.py
@@ -0,0 +1,85 @@
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+import h5py
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+import numpy as np
+from keras.utils import np_utils
+
+class DLModel():
+    def __init__(self, args):
+        self.dataset = args.dataset
+        self.saveDir = args.model
+        self.epochs = args.epochs
+        self.testFraction = args.test
+        self.batch_size = args.batch_size
+        # self.validation = args.validation
+        # self.fullModel = args.fullmodel
+    
+    def loadDataset(self):
+        self.f = h5py.File(self.dataset, 'r')
+        self.min_max_scaler = preprocessing.MinMaxScaler()
+        self.YEncoder = preprocessing.LabelEncoder()
+        self.YEncoder.fit( [ i for i in self.f['dataset/Y'] ] )
+        self.X = self.min_max_scaler.fit_transform( np.array( [ i for i in self.f['dataset/X'] ] ) )
+        self.EncodedY = self.YEncoder.transform( [ i for i in self.f['dataset/Y'] ] )
+        self.Y = np_utils.to_categorical( self.EncodedY )
+        self.F = [ i for i in self.f['dataset/F'] ]
+
+    def createModel(self):
+        self.nLabels = self.YEncoder.classes_
+        self.model = Sequential()
+        self.model.add( Dense(units = 5000, input_dim = len(self.F)) )
+        self.model.add( Activation('relu') )
+        self.model.add( Dropout(0.5) )
+        self.model.add( Dense(units = 2000) )
+        self.model.add( Activation('relu') )
+        self.model.add( Dropout(0.5) )
+        self.model.add( Dense(units = 1000) )
+        self.model.add( Activation('relu') )
+        self.model.add( Dropout(0.5) )
+        self.model.add( Dense(units = 200) )
+        self.model.add( Activation('relu') )
+        self.model.add( Dense(units = len(self.nLabels)) )
+        self.model.add( Activation('softmax') )
+
+        print( self.model.summary() )
+
+        self.model.compile(
+            loss = "categorical_crossentropy",
+            optimizer = "adam",
+            metrics = ["accuracy"]
+        )
+    
+    def trainModel(self):
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
+            self.X, 
+            self.Y, 
+            test_size = self.testFraction
+        )
+        self.model.fit(
+            self.x_train,
+            self.y_train,
+            epochs = self.epochs,
+            batch_size = self.batch_size,
+        )
+
+    def testModel(self):
+        score = self.model.evaluate(self.x_test, self.y_test, batch_size=self.batch_size)
+        print score
+    
+def main(args):
+    # create the object model
+    ML = DLModel(args)
+
+    # load the dataset
+    ML.loadDataset()
+
+    # create the deepLearning model
+    ML.createModel()
+
+    # train the deep learning model
+    ML.trainModel()
+
+    # test deep L model
+    ML.testModel()
