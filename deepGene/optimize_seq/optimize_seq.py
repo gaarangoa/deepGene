5545acd440216001cd8ed24d52763fc98434a9a4
dhoha <dabid@vt.edu>
2017-12-20 17:55:45 -0500

    add comments

--- deepGene/optimize_seq/optimize_seq.py
+++ deepGene/optimize_seq/optimize_seq.py
@@ -1,3 +1,7 @@
+"""
+Given the motifs that can be integrated to the wild sequence, this module takes care of changing the the wild nt
+sequence to match the motifs
+"""
 
 __version__ = '1.1.0'
 __author__ = 'Dhoha Abid'
@@ -9,7 +13,6 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
 from random import randint
-from random import sample
 import traceback
 from blast_iupac import BlastIUPAC
 from helper_functions import translate_nt_sequence
@@ -28,18 +31,34 @@ class OptimizeSeq:
         self.included_motifs = {}
 
     def run(self, d_motifs, f_name):
-        self.optimize_wild_seq(d_motifs)
-        self.check_optimized_seq()
-        self.write_optimized_seq(f_name)
+        """
+        This method joins all the pieces of this class together: optimizes the wild sequence, checks the resulting
+        sequence to be translated the same as the wild sequence. write the optimized sequence to fasta file and the
+        metadata to json file. The metadata has the information of what are the motif being included as well as their
+        locations
+        :param d_motifs: a dictionary that includes the motifs that can be integrated to the wild sequence:
+                {'seq_motif':[(optimized alignment which is the intersection of the motif and aa, corresponding
+                wild alignment, (aa index lower bound - inclusive, aa index upper bound -inclusive)), ]}
+        :param f_name: file name where the optimized sequence will be saved, results will be in the res directory
+        :return:
+        """
+        try:
+            self.optimize_wild_seq(d_motifs)
+            self.check_optimized_seq()
+            self.write_optimized_seq(f_name)
+        except:
+            traceback.print_exc()
+            sys.exit(1)
 
     def optimize_wild_seq(self, d_motifs):
         """
-        create a string of same length as input. Assign all its nt the N character. update these characters using
+        create a string of same length as input using the N nt. At a first step, we update these characters using
         the motifs, then complete the missing values  from the wild sequence. Sometimes the same motif can be integrated
-        using different nucleotide sequences, we pickup these sequences randomly. In a later step, we can be specific
-        :param d_motifs: motif sequence
+        using different nucleotide sequences, we pickup these nt randomly.
+        :param d_motifs: dictionary that includes the motifs that can be integrated to the wild sequence
         :return:
         """
+        # fill with the nt of the motifs
         for seq_motif, res in d_motifs.items():
             self.included_motifs[seq_motif] = []
             for opt, wild, aa_bound, nt_bound in res:
@@ -56,6 +75,8 @@ class OptimizeSeq:
                             traceback.print_exc()
                             sys.exit(1)
                     self.included_motifs[seq_motif].append((opt, wild, aa_bound, nt_bound))
+
+        # fill the rest with the nt of the wild sequence
         for i, opt, nt in zip([i for i in range(len(self.seq_wild))], self.seq_opt, self.seq_wild):
             if opt == 'N':
                 self.seq_opt[i] = nt
@@ -63,14 +84,11 @@ class OptimizeSeq:
 
     def check_optimized_seq(self):
         """
-        check if the sequence (can be string or Seq) translates to the same aa of the input (original) sequence
-        :param seq: nucleotide sequence (can be string or Seq)
-        :return: raise an error if the sequence does not translate to same aa of the input
+        check if the sequence translates to the same aa of the wild sequence
+        :return: raise an error if the sequence does not translate to same aa of the input, the program will stop
         """
-        if type(self.seq_opt).__name__ == 'str':
-            seq = Seq(self.seq_opt)
         try:
-            seq_translated = seq.translate()
+            seq_translated = Seq(self.seq_opt).translate()
             if seq_translated != translate_nt_sequence(self.seq_wild):
                 raise OptimizedSeqException(seq_translated)
         except OptimizedSeqException as e:
@@ -78,8 +96,8 @@ class OptimizeSeq:
             sys.exit(1)
 
     def write_optimized_seq(self, f_name):
-        with open('res/' + f_name + '.fasta', 'w') as f_seq_opt\
-                , open('res/' + f_name + '.json', 'w') as f_meta_seq_opt:
+        with open('res/' + f_name + '.fasta', 'w') as f_seq_opt,\
+                open('res/' + f_name + '.json', 'w') as f_meta_seq_opt:
             SeqIO.write(SeqRecord(Seq(self.seq_opt), id='liver.motif', description=f_name), f_seq_opt, 'fasta')
             dump(self.included_motifs, f_meta_seq_opt)
 
@@ -91,10 +109,7 @@ def run_with_random_motifs():
     b = BlastIUPAC(seq_nt)
     b.run_batch(l_motif)
     o = OptimizeSeq(seq_nt)
-    o.run(b.motifs_to_integrate, 'test')
-    # print len(b.res_motif_optimize)
-    # sample(b.res_motif_optimize, 15)
-    # TODO send the name of the file
+    o.run(b.motifs_to_integrate, 'f8.v1')
 
 
 if __name__ == '__main__':
