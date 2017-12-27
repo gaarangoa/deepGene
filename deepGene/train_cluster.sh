87cfd56f13f32c7b37232e3d9b0145dfad97b774
gaarangoa <gaarangoa@gmail.com>
2017-12-27 10:59:09 -0500

    updatex

--- deepGene/train_cluster.sh
+++ deepGene/train_cluster.sh
@@ -3,8 +3,8 @@
 #SBATCH -p normal_q
 #SBATCH -N 1
 #SBATCH -t 20:10:00
-#SBATCH --mem=200G
-#SBATCH --gres=gpu:pascal:2
+#SBATCH --mem=800G
+#SBATCH --gres=gpu:pascal:1
 
 echo "Allocated GPU with ID $CUDA_VISIBLE_DEVICES"
 # echo "Activate virtual environment: "
