261b2a28b32b7c801ea1f1ce5fc66d8a50cd98bb
gaarangoa <gaarangoa@gmail.com>
2017-12-27 10:44:21 -0500

    fix cluster run

--- deepGene/train_cluster.sh
+++ deepGene/train_cluster.sh
@@ -4,14 +4,16 @@
 #SBATCH -N 1
 #SBATCH -t 20:10:00
 #SBATCH --mem=200G
-#SBATCH --gres=gpu:pascal:1
+#SBATCH --gres=gpu:pascal:2
 
 echo "Allocated GPU with ID $CUDA_VISIBLE_DEVICES"
 # echo "Activate virtual environment: "
-export PYTHONNOUSERSITE=True
-source /work/newriver/gustavo1/deepLearning/bitPredEnv/bin/activate
+
+# source /work/newriver/gustavo1/deepLearning/bitPredEnv/bin/activate
+# export PYTHONNOUSERSITE=True
+source activate gustavo1
+# module load gcc cuda theano 
 
 cd /work/newriver/gustavo1/deepLearning/deepGene/deepGene/
-module load gcc/5.4.0 theano
 
 python DLModel1.py
